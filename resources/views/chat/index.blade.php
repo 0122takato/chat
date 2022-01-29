c75f53452318bddc6ec62916dfdc7e52a329c4ae
0122takato <takato.buran0122@icloud.com>
2022-01-30 00:10:38 +0900

    csslink追加

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -10,6 +10,7 @@ gem
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{env('APP_NAME')}}</title>
+        <link rel="stylesheet" href="/css/chat.css" type="text/css">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
