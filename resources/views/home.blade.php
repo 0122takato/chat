9f0464ad61f250bfbb2e9aed0e53e752bd698d34
0122takato <takato.buran0122@icloud.com>
2022-01-31 15:09:06 +0900

    修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,20 +14,14 @@
     </div>
 </div>
 
-<div class="comment-container row justify-content-center">
-    <div class="input-group comment-area">
-        <textarea class="form-control" placeholder="input massage" aria-label="With textarea"></textarea>
-        <form method="POST" action="{{route('add')}}">
-            @csrf
-            <div class="comment-container row justify-content-center">
-                <div class="input-group comment-area">
-                    <textarea class="form-control" id="comment" name="comment" placeholder="input massage"
-                        aria-label="With textarea"></textarea>
-                    <button type="submit" class="btn btn-outline-primary comment-btn">Submit</button>
-                </div>
-            </div>
-        </form>
+<form method="POST" action="{{route('add')}}">
+    @csrf
+    <div class="comment-container row justify-content-center">
+        <div class="input-group comment-area">
+            <textarea class="form-control" id="comment" name="comment" placeholder="input massage"
+                aria-label="With textarea"></textarea>
+            <button type="submit" class="btn btn-outline-primary comment-btn">Submit</button>
+        </div>
     </div>
-</div>
-
+</form>
 @endsection
