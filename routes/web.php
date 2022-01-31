c47544e4fa4b5639dd54c2ab83689c722e3c6a65
0122takato <takato.buran0122@icloud.com>
2022-01-31 14:00:51 +0900

    route消し忘れ

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::redirect('/', '/chat');
-Route::resource('/chat', ChatController::class);
